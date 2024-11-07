869de742931620992c5d767fb369a201c2d6859c
levinshub <levinsworkpace@gmail.com>
2024-11-07 09:05:28 +0530

    initial commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,13 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { RouterOutlet,RouterLink } from '@angular/router';
 import { HeaderComponent } from "./common/header/header.component";
 import { HomeComponent } from "./home/home.component";
-import { CommonModule } from '@angular/common';  
-
+import { FooterComponent } from "./common/footer/footer.component";
+import { ViewProductComponent } from './view-product/view-product.component';
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, HeaderComponent, HomeComponent,CommonModule],
+  imports: [RouterOutlet, HeaderComponent, HomeComponent, FooterComponent,ViewProductComponent,RouterLink],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,21 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { CarouselModule } from 'ngx-owl-carousel-o';
 import { OwlOptions } from 'ngx-owl-carousel-o';
-import { provideAnimations } from '@angular/platform-browser/animations';
-import {NgFor, NgForOf} from "@angular/common";
-import { CommonModule } from '@angular/common';  
+import { NgFor, NgForOf } from "@angular/common";
+import { Router } from '@angular/router';
 @Component({
   selector: 'app-home',
   standalone: true,
-  imports: [CarouselModule,NgFor,CommonModule],
+  imports: [CarouselModule, NgFor],
   templateUrl: './home.component.html',
   styleUrl: './home.component.scss',
-  providers: [
-    provideAnimations()
-  ]
 })
 
-export class HomeComponent {
+export class HomeComponent implements OnInit {
+
+
+  constructor(private _router: Router) { }
 
   images = [
     {
@@ -27,14 +26,128 @@ export class HomeComponent {
       id: "2"
     },
   ]
+
+  productImages = [
+    {
+      image: "/assets/product_images/1.png",
+      id: "1",
+      name: "Red Chili Powder",
+      actualPrice: "100",
+      discountPrice: "80",
+      discountPercent: "20%"
+    }, {
+      image: "/assets/product_images/2.png",
+      id: "2",
+      name: "Sirudhaniya puttu maavu",
+      actualPrice: "80",
+      discountPrice: "60",
+      discountPercent: "25%"
+    }, {
+      image: "/assets/product_images/3.png",
+      id: "3",
+      name: "Small Onion Thalippu Vadagam",
+      actualPrice: "60",
+      discountPrice: "30",
+      discountPercent: "50%"
+    }, {
+      image: "/assets/product_images/4.png",
+      id: "4",
+      name: "Sukku Malli Coffee Powder",
+      actualPrice: "150",
+      discountPrice: "130",
+      discountPercent: "15%"
+    }, {
+      image: "/assets/product_images/5.png",
+      id: "5",
+      name: "Ready Idiyappam mix Flour",
+      actualPrice: "180",
+      discountPrice: "150",
+      discountPercent: "10%"
+    }, {
+      image: "/assets/product_images/6.png",
+      id: "6",
+      name: "Kuzhambu Chili Powder",
+      actualPrice: "235",
+      discountPrice: "225",
+      discountPercent: "3%"
+    }, {
+      image: "/assets/product_images/7.png",
+      id: "7",
+      name: "Kollu Idli Podi",
+      actualPrice: "267",
+      discountPrice: "247",
+      discountPercent: "8%"
+    }, {
+      image: "/assets/product_images/8.png",
+      id: "8",
+      name: "Black Pepper Powder",
+      actualPrice: "361",
+      discountPrice: "349",
+      discountPercent: "5%"
+    }, {
+      image: "/assets/product_images/9.png",
+      id: "9",
+      name: "Readymade Murukku Maavu",
+      actualPrice: "246",
+      discountPrice: "223",
+      discountPercent: "12%"
+    },
+  ]
+
+  catImages = [
+    {
+      image: "/assets/category_image/11.png",
+      id: "1",
+      name: "Red Chili Powder",
+      actualPrice: "100",
+      discountPrice: "80",
+      discountPercent: "20%"
+    }, {
+      image: "/assets/category_image/12.png",
+      id: "2",
+      name: "Sirudhaniya puttu maavu",
+      actualPrice: "80",
+      discountPrice: "60",
+      discountPercent: "25%"
+    }, {
+      image: "/assets/category_image/13.png",
+      id: "3",
+      name: "Small Onion Thalippu Vadagam",
+      actualPrice: "60",
+      discountPrice: "30",
+      discountPercent: "50%"
+    }, {
+      image: "/assets/category_image/14.png",
+      id: "4",
+      name: "Sukku Malli Coffee Powder",
+      actualPrice: "150",
+      discountPrice: "130",
+      discountPercent: "15%"
+    }, {
+      image: "/assets/category_image/15.png",
+      id: "5",
+      name: "Ready Idiyappam mix Flour",
+      actualPrice: "180",
+      discountPrice: "150",
+      discountPercent: "10%"
+    }, {
+      image: "/assets/category_image/16.png",
+      id: "6",
+      name: "Kuzhambu Chili Powder",
+      actualPrice: "235",
+      discountPrice: "225",
+      discountPercent: "3%"
+    },
+  ]
+
   customOptions: OwlOptions = {
     loop: true,
     mouseDrag: true,
     touchDrag: true,
-    pullDrag: false,
+    pullDrag: true,
     dots: false,
     navSpeed: 1000,
-    navText: ['Next', 'Prev'],
+    navText: ['', ''],
     responsive: {
       0: {
         items: 1
@@ -49,7 +162,42 @@ export class HomeComponent {
         items: 1
       }
     },
-    nav: true
+    nav: false
   }
- 
+  customHotSalesOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: false,
+    dots: false,
+    navSpeed: 200,
+    navText: ['', ''],
+    responsive: {
+      0: {
+        items: 1
+      },
+      400: {
+        items: 2
+      },
+      740: {
+        items: 3
+      },
+      940: {
+        items: 5
+      }
+    },
+    nav: true
+
+  }
+
+  ngOnInit(): void {
+
+  }
+
+  viewProduct(productId: any) {
+    this._router.navigate(['/view-product'], { queryParams: { productId: productId } })
+  }
+  addToCart(productId: any) {
+    alert(productId + "nane")
+  }
 }
